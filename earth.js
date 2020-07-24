b1b0266412f063d7008dbaaab0771bc6235ec69d
prasurjya <65493249+PrasurjyaWhiteHatJr@users.noreply.github.com>
2020-07-24 17:54:49 +0530

    Add files via upload

---
+++ earth.js
@@ -0,0 +1,123 @@
+//bullet = createSprite(320, 500, 10, 10);
+//bullet = addImage("bullet", bimage);
+//bullet.setCollider("rectangle", 0, 0, 10, 10);
+//bullet = scale = 0.1;
+//bullet.x = mario.x;
+
+//edges = createNewEdges();
+
+//if keyDown("UP_ARROW")){
+//bullet.velocity = 10;
+//}
+
+//var = count = 0;
+
+//function draw() {
+//backGround(bgImage);
+
+//text("score: ", count, 250, 100);
+
+//if gameState===("PLAY") {
+
+  //count = count + Math.round(WorldFrameRate/60);
+//if keyDown = ("LEFT_ARROW"){
+//changePosition(-3, 0);
+//}
+
+//else if keyDown = ("RIGHT_ARROW"){
+    //changePosition(3, 0);
+//}
+
+//else if keyDown = ("UP_ARROW"){
+    //changePosition(0, -3);
+//}
+
+//else if keyDown = ("DOWN_ARROW"){
+    //changePosition(0, +3);
+//}
+
+//playSound("darwds1.mp3")
+//bullet.x = mario.x+80;
+//mario.x = mouse.x;
+
+//if (keyCode === 32) {
+  //bullet.velocityX = -10
+//}
+
+
+
+
+
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
